cfda8e73c353249eb4bbe79d33b90f13c3e3d199
Shougan Gu <dashoug001@gmail.com>
2026-04-18 17:06:16 -0400

    0SFT patch

--- tuning/training/unified_early_pipeline.py
+++ tuning/training/unified_early_pipeline.py
@@ -201,7 +201,7 @@ def _parse_args(argv=None):
 
     # SFT pass@k
     parser.add_argument("--sft-passk-targets", type=float, nargs="+", 
-                        default=[0.05,0.1,0.15,0.2,0.25,0.3,0.35,0.4,0.45,0.5,0.55,0.6,0.65,0.7,0.75,0.8,0.85,0.9,0.95])
+                        default=[0.0, 0.05,0.1,0.15,0.2,0.25,0.3,0.35,0.4,0.45,0.5,0.55,0.6,0.65,0.7,0.75,0.8,0.85,0.9,0.95])
     parser.add_argument("--sft-passk-early", type=parse_early_tuple, nargs="*",
                         default=[])
     parser.add_argument("--sft-passk-k-values", type=int, nargs="+", default=[1])
